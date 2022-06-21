1355e23b2f2546758a5efc15070c1a12fbef117e
Altaf Ansari <altafansari1037@gmail.com>
2022-06-21 18:52:20 +0530

    nasted object

--- src/page/ObjectMethods.js
+++ src/page/ObjectMethods.js
@@ -1,6 +1,12 @@
 let myObj = {
   name: "altaf",
   work: "developer",
+  didi: {
+    name: "kajol",
+    age: 24,
+    dob: "24/09/2022",
+    didiFriends: ["sonu", "Dheeraj", "altaf", "loki", "varsha", "ajay"],
+  },
   friends: "sonu monu tonu ",
   education: "BSC.CS",
   office: "Mirats Insights",
@@ -8,10 +14,13 @@ let myObj = {
 // console.log("my name --------->" + myObj[education]);
 let keys = Object.keys(myObj);
 console.log("keys--------> " + keys);
-let values = Object.values(myObj);
-console.log("values------> " + values);
-let entries = Object.entries(myObj);
-console.log("entries-----> " + entries);
-Object.keys(myObj).map((key) => {
-  console.log(key, "==>", myObj[key]);
-});
+// let values = Object.values(myObj);
+// console.log("values------> " + values);
+// let entries = Object.entries(myObj);
+// console.log("entries-----> " + entries);
+// -----------------nasted --------------
+// let keys2 = Object.keys(myObj.didi);
+// console.log("keys2--------> " + keys2);
+let values2 = Object.values(myObj.didi);
+
+console.log("values2------> " + values2);
